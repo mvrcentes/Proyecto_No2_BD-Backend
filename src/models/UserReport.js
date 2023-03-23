4f6ae5e7a24d8d09ab2ac8e766dc0fa93e5ad25b
Marco Ramirez <mvrcentes@hotmail.com>
2023-03-22 19:25:11 -0600

    update userReport model

--- src/models/UserReport.js
+++ src/models/UserReport.js
@@ -3,7 +3,8 @@ import Joi from 'joi'
 
 const UserReport = Joi.object({
     doctorName: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required(),
+    patientCUI: Joi.string().required(),
+    doctorEmail: Joi.string().required(),
+    doctorPhone: Joi.string().required(),
     note: Joi.string().required(), 
 })
